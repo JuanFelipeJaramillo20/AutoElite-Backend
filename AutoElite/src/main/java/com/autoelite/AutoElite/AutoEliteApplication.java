05be509cd3fa1a7eee6dfbcfcd28543b931dcb65
Juan Felipe Jaramillo <jfelipe.jaramillo@tcmpartners.com>
2023-05-18 16:45:08 -0500

    main => Añadiendo proyecto base

---
+++ AutoElite/src/main/java/com/autoelite/AutoElite/AutoEliteApplication.java
@@ -0,0 +1,13 @@
+package com.autoelite.AutoElite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoEliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoEliteApplication.class, args);
+	}
+
+}
---
+++ AutoElite/src/test/java/com/autoelite/AutoElite/AutoEliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoelite.AutoElite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoEliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
