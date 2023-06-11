c4f3a3ae041487283be2098ed56abc950b9b77b1
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2023-06-10 20:51:28 -0500

    Servicio de autenticacion para Publicaciones
    Signed-off-by: Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>

--- AutoElite/src/main/java/com/autoelite/AutoElite/Publicacion/Publicacion.java
+++ AutoElite/src/main/java/com/autoelite/AutoElite/Publicacion/Publicacion.java
@@ -1,6 +1,7 @@
 package com.autoelite.AutoElite.Publicacion;
 
 import com.autoelite.AutoElite.Carro.Carro;
+import com.autoelite.AutoElite.Usuarios.Usuario;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,6 +18,10 @@ public class Publicacion {
     private Long id;
     private String fechaPublicacion;
     private String ciudad;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_email", referencedColumnName = "email", nullable = false)
+    private Usuario usuarioPublicacion;
 
     @OneToOne
     @JoinColumn(name = "car_id", nullable = false)
--- AutoElite/src/main/java/com/autoelite/AutoElite/Publicacion/PublicacionService.java
+++ AutoElite/src/main/java/com/autoelite/AutoElite/Publicacion/PublicacionService.java
@@ -1,5 +1,8 @@
 package com.autoelite.AutoElite.Publicacion;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.sql.exec.spi.StandardEntityInstanceResolver;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,6 +11,8 @@ import java.util.Optional;
 @Service
 public class PublicacionService {
     private final PublicacionDAO publicacionDAO;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public PublicacionService(PublicacionDAO publicacionDAO) {
         this.publicacionDAO = publicacionDAO;
@@ -23,16 +28,24 @@ public class PublicacionService {
 
 
     public Publicacion getPublicacionesById(String id) {
-        return publicacionDAO.findById(id).orElseThrow(()->new RuntimeException("Publicacion no encontrada con id: "+id));
+        return publicacionDAO.findById(id).orElseThrow(() -> new RuntimeException("Publicacion no encontrada con id: " + id));
     }
 
-    public void addPublicaciones(Publicacion publicacion){
+    public List<Publicacion> getPublicacionesByEmail(String email) {
+        String queryString = "SELECT p FROM Publicacion p JOIN p.usuarioPublicacion u WHERE u.email = :userEmail";
+        return entityManager.createQuery(queryString, Publicacion.class)
+                .setParameter("userEmail", email)
+                .getResultList();
+    }
+
+    public void addPublicaciones(Publicacion publicacion) {
         publicacionDAO.save(publicacion);
     }
+
     public void updatePublicaciones(String id, Publicacion publicacion) {
         Optional<Publicacion> existingPublicaciones = publicacionDAO.findById(id);
-        if (existingPublicaciones.isPresent()){
-            if (publicacion.getCiudad() != null && !publicacion.getCiudad().isEmpty()){
+        if (existingPublicaciones.isPresent()) {
+            if (publicacion.getCiudad() != null && !publicacion.getCiudad().isEmpty()) {
                 existingPublicaciones.get().setCiudad(publicacion.getCiudad());
             }
             if (publicacion.getFechaPublicacion() != null && !publicacion.getFechaPublicacion().isEmpty()){
