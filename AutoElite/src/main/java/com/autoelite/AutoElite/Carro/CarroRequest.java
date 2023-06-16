868fe5f251825f94bfc7aea28b715811aa586c07
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2023-06-16 09:56:34 -0500

    Modificando controlador y servicio de publicacion para relacionar carro y usuario con publicacion
    Signed-off-by: Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>

---
+++ AutoElite/src/main/java/com/autoelite/AutoElite/Carro/CarroRequest.java
@@ -0,0 +1,29 @@
+package com.autoelite.AutoElite.Carro;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
+public class CarroRequest {
+    private String puertas;
+    private String motor;
+    private String ciudad;
+    private String marca;
+    private String placa;
+    private String color;
+    private String tipo;
+    private String combustible;
+    private String year;
+    private String estado;
+    private String transmision;
+    private Double precio;
+    private Integer kilometraje;
+    private Boolean precioEsNegociable;
+
+    // Constructors, getters, and setters
+}
