19e2acad578a26c27c81337f57e881b573276e91
davidArce <davidsteven654@gamil.com>
2023-06-25 10:44:43 -0500

    volviendo el properties a como estaba

--- AutoElite/src/main/java/com/autoelite/AutoElite/AutoEliteApplication.java
+++ AutoElite/src/main/java/com/autoelite/AutoElite/AutoEliteApplication.java
@@ -12,8 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @PropertySources({
         @PropertySource("classpath:application.properties"),
-        //@PropertySource("classpath:application-secret.properties"),
-        @PropertySource("classpath:application-local.properties")
+        @PropertySource("classpath:application-secret.properties"),
+        //@PropertySource("classpath:application-local.properties")
 })
 public class AutoEliteApplication {
 	public static void main(String[] args) {
